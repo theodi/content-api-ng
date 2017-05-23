0226410ad6860fa6918120623023c48b3e672fbb
Jez Higgins <jez@jezuk.co.uk>
2017-05-23 12:00:27 +0100

    Added tag_url, with_tag_url, and tag_web_url

--- test/unit/url_helper.js
+++ test/unit/url_helper.js
@@ -15,9 +15,23 @@ describe('URL Helper', () => {
 
   it('tag type', () => {
     equal(default_url.tag_type_url('fish'), 'http://example.org/tags/fish.json');
-    equal(default_url.tag_type_url('big_train'), 'http://example.org/tags/big_train.json');
+    equal(default_url.tag_type_url('big_train'), 'http://example.org/tags/big_trains.json');
 
     const tt = { 'singular': 'banana', 'plural': 'bananas' };
     equal(default_url.tag_type_url(tt), 'http://example.org/tags/bananas.json');
+  });
+
+  it('tag', () => {
+    const about = { tag_id: 'about', tag_type: 'section' };
+    equal(default_url.tag_url(about), 'http://example.org/tags/sections/about.json');
+    equal(default_url.with_tag_url(about), 'http://example.org/with_tag.json?section=about');
+    equal(default_url.tag_web_url(about), 'http://theodi.test/tags/about');
+
+    const halibut = { tag_id: 'halibut', tag_type: 'fish' };
+    equal(default_url.tag_url(halibut), 'http://example.org/tags/fish/halibut.json');
+    equal(default_url.with_tag_url(halibut), 'http://example.org/with_tag.json?fish=halibut');
+    equal(default_url.tag_web_url(halibut), 'http://theodi.test/tags/halibut');
+
+    equal(default_url.with_tag_url([halibut, about]), 'http://example.org/with_tag.json?fish=halibut&section=about');
   });
 });
