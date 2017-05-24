d59f0c12df96a77827709367684b3cfb281fa39b
Jez Higgins <jez@jezuk.co.uk>
2017-05-24 12:25:14 +0100

    extend with_tag to add in additional query params

--- test/unit/url_helper.js
+++ test/unit/url_helper.js
@@ -4,6 +4,9 @@ const url_helper = require('../../lib/url_helper.js');
 
 const default_url = url_helper('test');
 
+const about = { tag_id: 'about', tag_type: 'section' };
+const halibut = { tag_id: 'halibut', tag_type: 'fish' };
+
 describe('URL Helper', () => {
   it('app url', () => {
     equal(url_helper.custom('localhost').api_url(), 'http://localhost/');
@@ -22,16 +25,17 @@ describe('URL Helper', () => {
   });
 
   it('tag', () => {
-    const about = { tag_id: 'about', tag_type: 'section' };
     equal(default_url.tag_url(about), 'http://example.org/tags/sections/about.json');
-    equal(default_url.with_tag_url(about), 'http://example.org/with_tag.json?section=about');
     equal(default_url.tag_web_url(about), 'http://theodi.test/tags/about');
 
-    const halibut = { tag_id: 'halibut', tag_type: 'fish' };
     equal(default_url.tag_url(halibut), 'http://example.org/tags/fish/halibut.json');
-    equal(default_url.with_tag_url(halibut), 'http://example.org/with_tag.json?fish=halibut');
     equal(default_url.tag_web_url(halibut), 'http://theodi.test/tags/halibut');
+  });
 
+  it('with_tag', () => {
+    equal(default_url.with_tag_url(about), 'http://example.org/with_tag.json?section=about');
+    equal(default_url.with_tag_url(halibut), 'http://example.org/with_tag.json?fish=halibut');
     equal(default_url.with_tag_url([halibut, about]), 'http://example.org/with_tag.json?fish=halibut&section=about');
+    equal(default_url.with_tag_url(about, { 'sort': 'title' }), 'http://example.org/with_tag.json?section=about&sort=title');
   });
 });
