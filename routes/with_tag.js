f23e57f9ed4d6b652208d06553e615eae7831f1d
Jez Higgins <jez@jezuk.co.uk>
2017-05-24 12:06:53 +0100

    start filling out with_tags.json?tag=...

--- routes/with_tag.js
+++ routes/with_tag.js
@@ -1,15 +1,47 @@
 const result_set = require('./result_set.js');
 const error_404 = require('./error_404.js');
 const content_types = require('../presenters/content_types.js');
+const tags = require('../presenters/tags.js');
+const singular = require('pluralize').singular;
 
-function handle_tag_param(tag, db, url_helper) {
+const modifiers = [
+  'sort',
+  'author',
+  'node',
+  'organization_name',
+  'role',
+  'whole_body',
+  'page',
+  'order_by',
+  'summary'
+];
 
+function modifier_params(req) {
+  const m = {};
+  for (const p of modifiers)
+    if (req.query[p])
+      m[p] = req.param[p];
+  return m;
+} // modifier_params
+
+//////////////////////////////////////////////////////////////
+async function handle_tag_param(req, res, db, url_helper) {
+  const tag = req.query["tag"];
+  const possible_tags = await tags.by_ids(tag, db);
+
+  const modifiers = modifier_params(req);
+  // If we can unambiguously determine the tag, redirect to its correct URL
+  if (possible_tags.length == 1)
+    res.redirect(url_helper.with_tag_url(possible_tags, modifiers));
+  else if (content_types.some(ct => ct == singular(tag)))
+    res.redirect(url_helper.with_tag_url(tag, modifiers));
+  else
+    error_404(res);
 } // handle_tag_param
 
 function with_tag_formatter(req, res, db, url_helper) {
-  const tag = req.query["tag"];
-  if (tag)
-    handle_tag_param(tag, db, url_helper);
+  if (req.query["tag"])
+    return handle_tag_param(req, res, db, url_helper);
 
   error_404(res);
 } // with_tags_formatter
--- test/integration/with_tag.js
+++ test/integration/with_tag.js
@@ -6,6 +6,7 @@ const monk = require('monk');
 const mongo_config = require('config').get('mongo');
 
 const db = monk(`${mongo_config.hostname}:${mongo_config.port}/${mongo_config.database}`);
+const tags_collection = db.get('tags');
 
 describe('With Tag', () => {
   expect_404('no tag query param');
@@ -13,29 +14,67 @@ describe('With Tag', () => {
   expect_404('unknown tag', '?tag=monkeypunks');
   expect_404('multiple tags found', '?tag=ambiguity');
 
-  it('ignore keywords', done => {
-    request.
-      get('/with_tag.json?tag=farmers').
-      expect(302).
-      expect('Location', 'http://www.example.org/with_tag.json?section=farmers').
-      end(done);
-  }); // ignore keywords
+  expect_redirect('ignore keywords', '?tag=farmers', '?section=farmers');
+  expect_redirect('to typed url', '?tag=trout', '?article=trout');
 
-  it('redirect to typed URL even when zero results', done => {
-    request.
-      get('/with_tag.json?tag=trout').
-      expect(302).
-      expect('Location', 'http://www.example.org/with_tag.json?article=farmers').
-      end(done);
+  /////////////////////////////////////////
+  before(() => {
+    tags_collection.insert(test_tag_data);
   });
 
+  after(() => {
+    tags_collection.remove({});
+  });
 });
 
+const test_tag_data = [
+  // ambiguous tag
+  {
+    'title': 'Ambiguity',
+    'tag_type': 'section',
+    'tag_id': 'ambiguity'
+  },
+  {
+    'title': 'Ambiguity',
+    'tag_type': 'article',
+    'tag_id': 'ambiguity'
+  },
+
+  // unambiguous, keywords are ignored
+  {
+    'title': 'Farmers',
+    'tag_type': 'section',
+    'tag_id': 'farmers'
+  },
+  {
+    'title': 'Farmers',
+    'tag_type': 'keyword',
+    'tag_id': 'farmers'
+  },
+
+  {
+    'title': 'Trout',
+    'tag_type': 'article',
+    'tag_id': 'trout'
+  }
+];
+
+////////////////////////////////////////
 function expect_404(label, query = '') {
-  it('404 when ' + label, done => {
+  it(`404 when ${label}`, done => {
     request.
       get(`/with_tag.json${query}`).
       expect(404).
       end(done);
   });
 } // expect_404
+
+function expect_redirect(label, query, location) {
+  it(`redirect when ${label}`, done => {
+    request.
+      get(`/with_tag.json${query}`).
+      expect(302).
+      expect('Location', `http://example.org/with_tag.json${location}`).
+      end(done);
+  });
+} // expect redirect
