aaf9c601321f5ff547682a7ee5d1164fdd5695f4
Jez Higgins <jez@jezuk.co.uk>
2017-05-24 10:07:50 +0100

    More with_tag tests

--- test/integration/with_tag.js
+++ test/integration/with_tag.js
@@ -8,17 +8,33 @@ const mongo_config = require('config').get('mongo');
 const db = monk(`${mongo_config.hostname}:${mongo_config.port}/${mongo_config.database}`);
 
 describe('With Tag', () => {
-  expect_404('no tag query param', '/with_tag.json');
-  expect_404('empty tag query param', '/with_tag.json?tag=');
-  expect_404('unknown tag', '/with_tag.json?tag=farmers');
+  expect_404('no tag query param');
+  expect_404('empty tag query param', '?tag=');
+  expect_404('unknown tag', '?tag=monkeypunks');
+  expect_404('multiple tags found', '?tag=ambiguity');
 
+  it('ignore keywords', done => {
+    request.
+      get('/with_tag.json?tag=farmers').
+      expect(302).
+      expect('Location', 'http://www.example.org/with_tag.json?section=farmers').
+      end(done);
+  }); // ignore keywords
+
+  it('redirect to typed URL even when zero results', done => {
+    request.
+      get('/with_tag.json?tag=trout').
+      expect(302).
+      expect('Location', 'http://www.example.org/with_tag.json?article=farmers').
+      end(done);
+  });
 
 });
 
-function expect_404(label, url) {
+function expect_404(label, query = '') {
   it('404 when ' + label, done => {
     request.
-      get(url).
+      get(`/with_tag.json${query}`).
       expect(404).
       end(done);
   });
