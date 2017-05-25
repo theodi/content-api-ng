9ca5f6b1cafc27e94657d0c57e0fc00ca9651f17
Jez Higgins <jez@jezuk.co.uk>
2017-05-25 11:06:29 +0100

    url_helper.artefact_url & artefact_web_url

--- test/unit/url_helper.js
+++ test/unit/url_helper.js
@@ -2,11 +2,14 @@ const equal = require('assert').equal;
 
 const url_helper = require('../../lib/url_helper.js');
 
-const default_url = url_helper('test');
+const du = url_helper('test');
 
-const about = { tag_id: 'about', tag_type: 'section' };
-const halibut = { tag_id: 'halibut', tag_type: 'fish' };
-const trout = { tag_id: 'trout', tag_type: 'fish' };
+const about_tag = { 'tag_id': 'about', 'tag_type': 'section' };
+const halibut_tag = { 'tag_id': 'halibut', 'tag_type': 'fish' };
+const trout_tag = { 'tag_id': 'trout', 'tag_type': 'fish' };
+
+const brick_artefact = { 'slug': 'brick' };
+const mortar_artefact = { 'slug': 'mortar', 'rendering_path': '/render-with-lime' };
 
 describe('URL Helper', () => {
   it('app url', () => {
@@ -18,31 +21,45 @@ describe('URL Helper', () => {
   });
 
   it('tag type', () => {
-    equal(default_url.tag_type_url('fish'), 'http://example.org/tags/fish.json');
-    equal(default_url.tag_type_url('big_train'), 'http://example.org/tags/big_trains.json');
+    url_equal(du.tag_type_url('fish'), 'tags/fish.json');
+    url_equal(du.tag_type_url('big_train'), 'tags/big_trains.json');
 
     const tt = { 'singular': 'banana', 'plural': 'bananas' };
-    equal(default_url.tag_type_url(tt), 'http://example.org/tags/bananas.json');
+    url_equal(du.tag_type_url(tt), 'tags/bananas.json');
   });
 
   it('tag', () => {
-    equal(default_url.tag_url(about), 'http://example.org/tags/sections/about.json');
-    equal(default_url.tag_web_url(about), 'http://theodi.test/tags/about');
+    url_equal(du.tag_url(about_tag), 'tags/sections/about.json');
+    url_equal(du.tag_web_url(about_tag), 'http://theodi.test/tags/about');
 
-    equal(default_url.tag_url(halibut), 'http://example.org/tags/fish/halibut.json');
-    equal(default_url.tag_web_url(halibut), 'http://theodi.test/tags/halibut');
+    url_equal(du.tag_url(halibut_tag), 'tags/fish/halibut.json');
+    url_equal(du.tag_web_url(halibut_tag), 'http://theodi.test/tags/halibut');
   });
 
   it('with_tag', () => {
-    equal(default_url.with_tag_url(about), 'http://example.org/with_tag.json?section=about');
-    equal(default_url.with_tag_url(halibut), 'http://example.org/with_tag.json?fish=halibut');
-    equal(default_url.with_tag_url([halibut, about]), 'http://example.org/with_tag.json?fish=halibut&section=about');
-    equal(default_url.with_tag_url([halibut, trout]), 'http://example.org/with_tag.json?fish=halibut%2Ctrout');
-    equal(default_url.with_tag_url(about, { 'sort': 'title' }), 'http://example.org/with_tag.json?section=about&sort=title');
+    url_equal(du.with_tag_url(about_tag), 'with_tag.json?section=about');
+    url_equal(du.with_tag_url(halibut_tag), 'with_tag.json?fish=halibut');
+    url_equal(du.with_tag_url([halibut_tag, about_tag]), 'with_tag.json?fish=halibut&section=about');
+    url_equal(du.with_tag_url([halibut_tag, trout_tag]), 'with_tag.json?fish=halibut%2Ctrout');
+    url_equal(du.with_tag_url(about_tag, { 'sort': 'title' }), 'with_tag.json?section=about&sort=title');
   });
 
   it('with_type', () => {
-    equal(default_url.with_type_url('muffin'), 'http://example.org/with_tag.json?type=muffin');
-    equal(default_url.with_type_url('truffle', { 'sort': 'title' }), 'http://example.org/with_tag.json?type=truffle&sort=title');
+    url_equal(du.with_type_url('muffin'), 'with_tag.json?type=muffin');
+    url_equal(du.with_type_url('truffle', { 'sort': 'title' }), 'with_tag.json?type=truffle&sort=title');
+  });
+
+  it('artefact', () => {
+    url_equal(du.artefact_url(brick_artefact), 'brick.json');
+    url_equal(du.artefact_web_url(brick_artefact), 'http://theodi.test/brick');
+
+    url_equal(du.artefact_url(mortar_artefact), 'mortar.json');
+    url_equal(du.artefact_web_url(mortar_artefact), 'http://theodi.test/render-with-lime');
   });
 });
+
+function url_equal(actual, url_or_path) {
+  const expected = url_or_path.startsWith('http') ? url_or_path : `http://example.org/${url_or_path}`;
+
+  equal(actual, expected);
+} // url_equal
