3e50855526806f3cea494a9e12d135a9e8bcbe39
Jez Higgins <jez@jezuk.co.uk>
2017-05-30 11:13:43 +0100

    /latest.json?type= done

--- routes/latest.js
+++ routes/latest.js
@@ -1,7 +1,9 @@
 const error_404 = require('./error_404.js');
 const single_artefact = require('./single_artefact.js');
+const Content_types = require('../mongo_documents/content_types.js');
 const Tags = require('../mongo_documents/tags.js');
 const Artefacts = require('../mongo_documents/artefacts.js');
+const singular = require('pluralize').singular;
 
 async function latest_formatter(req, res, db, url_helper) {
   const slug = await find_and_verify_slug(db, req);
@@ -24,22 +26,33 @@ function find_and_verify_slug(db, req) {
   const role = req.query["role"];
 
   if (type)
-    return verify_type_and_find_slug(db, type);
+    return verify_type_and_find_slug(db, singular(type), role);
   if (tag)
     return verify_tag_and_find_slug(db, tag, role);
   return null;
 } // find_and_verify_slug
 
-function verify_type_and_find_slug(db, type) {
+async function verify_type_and_find_slug(db, type, role) {
+  if (!Content_types.some(ct => ct == type))
+    return;
+
+  return await latest_artefact_slug(db, Artefacts.by_type, type, role);
 } // verify_type_and_find_slug
 
 async function verify_tag_and_find_slug(db, tag, role) {
-  const possible_tags = await Tags.by_ids(tag, db);
+  const possible_tags = await Tags.by_ids(tag, db);  // I think this check may be redundant
   if (possible_tags.length == 0)
     return;
 
-  const artefacts = await Artefacts.by_tags(db, tag, role,
-		  			    { sort: 'date', limit: 1});
+  return await latest_artefact_slug(db, Artefacts.by_tags, tag, role);
+} // verify_tag_and_find_slug
+
+async function latest_artefact_slug(db, query_method, param, role) {
+  const artefacts = await query_method(db, param, role,
+		  		       { sort: 'date', limit: 1});
+  if (!artefacts.length)
+    return;
+
   const artefact = artefacts[0];
   return artefact.slug;
-} // verify_tag_and_find_slug
+} // latest_artefact_slug
