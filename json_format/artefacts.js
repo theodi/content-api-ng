c44d94e309f45907e50761c41938d11c940cb6c2
Jez Higgins <jez@jezuk.co.uk>
2017-06-02 15:20:49 +0100

    reverted to showdown

--- json_format/artefacts.js
+++ json_format/artefacts.js
@@ -1,7 +1,7 @@
 const stream_of = require('rillet').of;
 const stream_from = require('rillet').from;
 const tag_format = require('./tags.js');
-const remarkable = require('remarkable');
+const showdown = require('showdown');
 
 function edition_or_artefact(artefact, edition_field, artefact_field) {
   if (artefact.edition && artefact.edition[edition_field])
@@ -185,8 +185,8 @@ function attachmentImage(v) {
 function convertIfGovspeak(f, v) {
   if (GOVSPEAK.indexOf(f) == -1)
     return [f, v];
-  converter = new remarkable({'html': true});
-  return [f, converter.render(v)];
+  converter = new showdown.Converter();
+  return [f, converter.makeHtml(v)];
 } // convertIfGovspeak
 
 function convertIfDate(f, v) {
