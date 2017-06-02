4e9a3a390a1dacc3ebde16f723389cb752c57b07
Jez Higgins <jez@jezuk.co.uk>
2017-06-02 16:22:55 +0100

    tidy

--- routes/tags.js
+++ routes/tags.js
@@ -1,7 +1,7 @@
 const tags = require('../mongo_documents/tags.js');
 const error_503 = require('./error_503.js');
 
-function tags_json_formatter(req, res, db, url_helper) {
+function tags_formatter(req, res, db, url_helper) {
   const tag_type = req.query['type'];
   const label = tag_type ? `${tag_type} tags` : "All tags";
 
@@ -11,11 +11,11 @@ function tags_json_formatter(req, res, db, url_helper) {
       console.log(err);
       error_503(res);
     });
-} // tags_json_formatter
+} // tags_formatter
 
-function make_tag_json_formatter(db, url_helper) {
+function make_tag_formatter(db, url_helper) {
   return (req, res) =>
-    tags_json_formatter(req, res, db, url_helper);
-} // make_tag_json_formatter
+    tags_formatter(req, res, db, url_helper);
+} // make_tag_formatter
 
-module.exports = make_tag_json_formatter;
+module.exports = make_tag_formatter;
